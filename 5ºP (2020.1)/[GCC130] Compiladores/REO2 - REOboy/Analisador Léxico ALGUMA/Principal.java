5b81033645e7e9b51da24085b24393c57af44d07
Phumacinha <pedro.souza2@estudante.ufla.br>
2020-06-22 00:01:48 -0300

    leitura do nome do arquivo dado por entrada do usuário

--- 5ºP (2020.1)/[GCC130] Compiladores/REO2 - REOboy/Analisador Léxico ALGUMA/Principal.java
+++ 5ºP (2020.1)/[GCC130] Compiladores/REO2 - REOboy/Analisador Léxico ALGUMA/Principal.java
@@ -2,27 +2,31 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws IOException {
-        InputStream is = new FileInputStream("ALGUMA/somafibonacci.alguma");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Nome do arquivo: ");
+        String arquivo = in.next();
+        InputStream is = new FileInputStream("ALGUMA/"+arquivo);
         InputStreamReader isr = new InputStreamReader(is);
 
-        Lexico lexico = new Lexico();
-        //...
-        lexico.setInput(isr);
-        //...
+        Lexico lexico = new Lexico(isr);
         try
         {
             Token t = null;
             while ( (t = lexico.nextToken()) != null )
             {
-                System.out.println(t.getLexeme());
+                System.out.println(t.toString());
             }
         }
         catch ( LexicalError e )
         {
-            System.err.println(e.getMessage() + "e;, em " + e.getPosition());
+            System.err.println("ERRO LÉXICO ( " + e.getMessage() + " ) @ " + e.getPosition());
         }
     }
 }
+
+
+
